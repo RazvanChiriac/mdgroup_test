490e5985b5617b67c1dc0514fedf647a4cc0bc69
Razvan <chiriac.razvan.eduard@gmail.com>
2022-01-27 16:44:32 +0000

    basic navigator setup

---
+++ project/features/home/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export const Home = props => {
+    return (
+        <View style={{ backgroundColor: "red", height: 100, width: "100%" }}>
+            <Text>Test</Text>
+        </View>
+    );
+};
